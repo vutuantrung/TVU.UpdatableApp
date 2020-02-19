aa2bf8619e7cc93f7746a196c6cbe608b496f04f
TuanTrungVU <Trung.lespace@gmail.com>
2020-02-19 15:00:52 +0100

    adding interface

---
+++ CSharpUpdate/CustomLibrary/ISharpUpdatable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CustomLibrary
+{
+    public interface ISharpUpdatable
+    {
+        string ApplicationName { get; }
+        string ApplicationID { get; }
+        Assembly ApplicationAssembly { get; }
+        Uri UpdateXmlApplication { get; }
+        Form Context { get; }
+    }
+}
