58ee6c95ebe24a1569820fe6a33f11fea27d945f
TuanTrungVU <Trung.lespace@gmail.com>
2020-02-19 17:55:34 +0100

    Add event Handler for webclient and background worker

--- CSharpUpdate/SharpUpdate/SharpUpdateDownloadForm.cs
+++ CSharpUpdate/SharpUpdate/SharpUpdateDownloadForm.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,63 @@ namespace SharpUpdate
 {
     public partial class SharpUpdateDownloadForm : Form
     {
-        public SharpUpdateDownloadForm()
+        private WebClient webClient;
+
+        private BackgroundWorker bgWorker;
+
+        private string tempFile;
+
+        private string md5;
+
+        internal string TempFilePath
+        {
+            get { return this.tempFile; }
+        }
+
+        public SharpUpdateDownloadForm(Uri location, string md5)
         {
             InitializeComponent();
+
+            tempFile = Path.GetTempFileName();
+            this.md5 = md5;
+
+            webClient = new WebClient();
+            webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+            webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+
+            bgWorker = new BackgroundWorker();
+            bgWorker.DoWork += BgWorker_DoWork;
+            bgWorker.RunWorkerCompleted += BgWorker_RunWorkerCompleted;
+
+            try
+            {
+                webClient.DownloadFileAsync(location, this.tempFile);
+            }
+            catch
+            {
+                this.DialogResult = DialogResult.No;
+                this.Close();
+            }
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WebClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
