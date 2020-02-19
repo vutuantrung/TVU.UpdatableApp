00c6f47c7d376a14821a8736f6b6816bd16146a0
TuanTrungVU <Trung.lespace@gmail.com>
2020-02-19 15:01:29 +0100

    Add winforms

---
+++ CSharpUpdate/AppSharp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AppSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblTesting = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(85, 220);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(104, 34);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "Check for update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lblTesting
+            // 
+            this.lblTesting.AutoSize = true;
+            this.lblTesting.Location = new System.Drawing.Point(115, 119);
+            this.lblTesting.Name = "lblTesting";
+            this.lblTesting.Size = new System.Drawing.Size(35, 13);
+            this.lblTesting.TabIndex = 1;
+            this.lblTesting.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 266);
+            this.Controls.Add(this.lblTesting);
+            this.Controls.Add(this.btnUpdate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label lblTesting;
+    }
+}
+
---
+++ CSharpUpdate/AppSharp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using CustomLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppSharp
+{
+    public partial class Form1 : Form, ISharpUpdatable
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.lblTesting.Text = this.ApplicationAssembly.GetName().Version.ToString();
+        }
+
+        public string ApplicationName
+        {
+            get { return "AppSharp"; }
+        }
+
+        public string ApplicationID
+        {
+            get { return "AppSharp"; }
+        }
+
+        public Assembly ApplicationAssembly
+        {
+            get { return Assembly.GetExecutingAssembly(); }
+        }
+
+        public Uri UpdateXmlApplication
+        {
+            get { return new Uri(""); }
+        }
+
+        public Form Context
+        {
+            get { return this; }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
