cde83ed6e322ebf6514197271a9cf4ee7523cd2b
TuanTrungVU <Trung.lespace@gmail.com>
2020-02-19 15:14:58 +0100

    Implement internal class XML

---
+++ CSharpUpdate/CustomLibrary/SharpUpdateXml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+
+namespace CustomLibrary
+{
+    internal class SharpUpdateXml
+    {
+        private Version version;
+        private Uri uri;
+        private string fileName;
+        private string md5;
+        private string description;
+        private string launchArgs;
+
+        internal Version Version
+        {
+            get { return this.version; }
+        }
+
+        internal Uri Uri
+        {
+            get { return this.uri; }
+        }
+
+        internal string FileName
+        {
+            get { return this.fileName; }
+        }
+
+        internal string MD5
+        {
+            get { return this.md5; }
+        }
+
+        internal string Description
+        {
+            get { return this.description; }
+        }
+
+        internal string LauchArgs
+        {
+            get { return this.launchArgs; }
+        }
+
+        internal SharpUpdateXml(Version version, Uri uri, string fileName, string md5, string description, string launchArgs)
+        {
+            this.version = version;
+            this.uri = uri;
+            this.fileName = fileName;
+            this.md5 = md5;
+            this.description = description;
+            this.launchArgs = launchArgs;
+        }
+    }
+}
