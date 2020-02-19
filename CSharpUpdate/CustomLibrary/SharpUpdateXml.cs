4267df572bf5855a1f08d4f1346999b5624008e3
TuanTrungVU <Trung.lespace@gmail.com>
2020-02-19 15:34:27 +0100

    Adding methods
    _Add IsNewerThan
    _Add ExistOnServer
    _Add SharpUpdateXml

--- CSharpUpdate/CustomLibrary/SharpUpdateXml.cs
+++ CSharpUpdate/CustomLibrary/SharpUpdateXml.cs
@@ -52,5 +52,55 @@ namespace CustomLibrary
             this.description = description;
             this.launchArgs = launchArgs;
         }
+
+        internal bool IsNewerThan(Version version)
+        {
+            return this.version > version;
+        }
+
+        internal static bool ExistsOnServer(Uri location)
+        {
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(location.AbsoluteUri);
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                res.Close();
+
+                return res.StatusCode == HttpStatusCode.OK;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        internal static SharpUpdateXml Parse(Uri location, string appID)
+        {
+            Version version = null;
+            string url = string.Empty, fileName = string.Empty, md5 = string.Empty, description = string.Empty, launchArgs = string.Empty;
+
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(location.AbsoluteUri);
+
+                // Find the node update with the specific attribut appID
+                XmlNode node = doc.DocumentElement.SelectSingleNode($"//update[@appID='{appID}'");
+
+                if (node == null) return null;
+
+                version = Version.Parse(node["lastestVersion"].InnerText);
+                url = node["lastestVersionUrl"].InnerText;
+                fileName = node["fileName"].InnerText;
+                description = node["description"].InnerText;
+                launchArgs = node["launchArgs"].InnerText;
+
+                return new SharpUpdateXml(version, new Uri(url), fileName, md5, description, launchArgs);
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
