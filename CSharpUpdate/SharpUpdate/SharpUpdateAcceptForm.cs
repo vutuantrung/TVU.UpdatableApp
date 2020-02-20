5924fb86acc73763f577acd256fa82584bf1bf58
TuanTrungVU <Trung.lespace@gmail.com>
2020-02-20 10:27:53 +0100

    Add comments for each method

--- CSharpUpdate/SharpUpdate/SharpUpdateAcceptForm.cs
+++ CSharpUpdate/SharpUpdate/SharpUpdateAcceptForm.cs
@@ -10,14 +10,31 @@ using System.Windows.Forms;
 
 namespace SharpUpdate
 {
+    /// <summary>
+    /// Form to prompt the user to accept the update
+    /// </summary>
     internal partial class SharpUpdateAcceptForm : Form
     {
+        /// <summary>
+        /// The program to update's infor
+        /// </summary>
         private ISharpUpdatable applicationInfo;
 
+        /// <summary>
+        /// The update info from the update.xml
+        /// </summary>
         private SharpUpdateXml updateInfo;
 
+        /// <summary>
+        /// The update info display form
+        /// </summary>
         private SharpUpdateInfoForm updateInfoForm;
 
+        /// <summary>
+        /// Creates a new SharpUpdateAcceptForm
+        /// </summary>
+        /// <param name="applicationInfo"></param>
+        /// <param name="updateInfo"></param>
         internal SharpUpdateAcceptForm(ISharpUpdatable applicationInfo, SharpUpdateXml updateInfo)
         {
             InitializeComponent();
@@ -25,23 +42,34 @@ namespace SharpUpdate
             this.applicationInfo = applicationInfo;
             this.updateInfo = updateInfo;
 
+            // Assigns the icon if ot isn't null
             this.Text = this.applicationInfo.ApplicationName + " - Update available";
 
+            // Adds the update version # to the form
             this.lblNewVersion.Text = $"New version: {this.updateInfo.Version.ToString()}";
         }
 
+        /// <summary>
+        /// Button Yes click's event
+        /// </summary>
         private void btnYes_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.Yes;
             this.Close();
         }
 
+        /// <summary>
+        /// Button No click's event
+        /// </summary>
         private void btnNo_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.No;
             this.Close();
         }
 
+        /// <summary>
+        /// Button Details click's event
+        /// </summary>
         private void btnDetails_Click(object sender, EventArgs e)
         {
             if (this.updateInfoForm == null)
