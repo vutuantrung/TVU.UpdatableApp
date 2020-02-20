79c135f979a542b4bf22247634f3415c20b5008f
TuanTrungVU <Trung.lespace@gmail.com>
2020-02-20 10:11:01 +0100

    Update methods

--- CSharpUpdate/SharpUpdate/SharpUpdateAcceptForm.cs
+++ CSharpUpdate/SharpUpdate/SharpUpdateAcceptForm.cs
@@ -12,9 +12,22 @@ namespace SharpUpdate
 {
     internal partial class SharpUpdateAcceptForm : Form
     {
-        internal SharpUpdateAcceptForm()
+        private ISharpUpdatable applicationInfo;
+
+        private SharpUpdateXml updateInfo;
+
+        private SharpUpdateInfoForm updateInfoForm;
+
+        internal SharpUpdateAcceptForm(ISharpUpdatable applicationInfo, SharpUpdateXml updateInfo)
         {
             InitializeComponent();
+
+            this.applicationInfo = applicationInfo;
+            this.updateInfo = updateInfo;
+
+            this.Text = this.applicationInfo.ApplicationName + " - Update available";
+
+            this.lblNewVersion.Text = $"New version: {this.updateInfo.Version.ToString()}";
         }
     }
 }
